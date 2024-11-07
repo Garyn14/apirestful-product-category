3cc20790a9d89e73382c3a0b5874648b260845b1
Garyn14 <garyn.fernando.hr@gmail.com>
2024-11-06 21:33:46 -0500

    error catalog

---
+++ src/main/java/com/service/apirestful/product/product_service/util/ErrorCatalog.java
@@ -0,0 +1,20 @@
+package com.service.apirestful.product.product_service.util;
+
+import lombok.Getter;
+
+@Getter
+public enum ErrorCatalog {
+
+    PRODUCT_NOT_FOUND("ERR_PROD_001", "Product not found"),
+    INVALID_PRODUCT("ERR_PROD_002", "invalid product parameters"),
+    CATEGORY_NOT_FOUND("ERR_CAT_001", "Category not found"),
+    GENERIC_ERROR("ERR_GEN_001", "An unexpected error occurred");
+
+    private final String code;
+    private final String message;
+
+    ErrorCatalog(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+}
