af9ea68c89847160f70f38be0a8c930b71f308a0
Garyn14 <garyn.fernando.hr@gmail.com>
2024-11-06 21:00:48 -0500

    Implements service

--- src/main/java/com/service/apirestful/product/product_service/service/ProductServiceImpl.java
+++ src/main/java/com/service/apirestful/product/product_service/service/ProductServiceImpl.java
@@ -1,40 +1,89 @@
 package com.service.apirestful.product.product_service.service;
 
+import com.service.apirestful.product.product_service.mapper.ProductMapper;
 import com.service.apirestful.product.product_service.model.dto.CreateProductRequest;
 import com.service.apirestful.product.product_service.model.dto.ProductResponse;
+import com.service.apirestful.product.product_service.model.entity.Product;
+import com.service.apirestful.product.product_service.repository.CategoryRepository;
+import com.service.apirestful.product.product_service.repository.ProductRepository;
+import exception.CategoryNotFoundException;
+import exception.ProductNotFoundException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class ProductServiceImpl implements ProductService{
+
+    private final CategoryRepository categoryRepository;
+    private final ProductRepository productRepository;
+    private final ProductMapper productMapper;
+
     @Override
     public ProductResponse findById(Long id) {
-        return null;
+        return productRepository.findById(id)
+                .map(productMapper::toProductResponse)
+                .orElseThrow(ProductNotFoundException::new);
     }
 
     @Override
     public List<ProductResponse> findAll() {
-        return null;
+        return productRepository.findAll()
+                .stream()
+                .map(productMapper::toProductResponse)
+                .toList();
     }
 
     @Override
     public List<ProductResponse> findAllByCategoryId(Long categoryId) {
-        return null;
+        return categoryRepository.findById(categoryId)
+                .map(productRepository::findAllByCategory)
+                .map(products -> products.stream()
+                        .map(productMapper::toProductResponse)
+                        .toList())
+                .orElseThrow(CategoryNotFoundException::new);
     }
 
     @Override
     public ProductResponse save(CreateProductRequest request) {
-        return null;
+        return categoryRepository.findById(request.getCategoryId())
+                .map(category -> {
+                    Product product = new Product();
+                    product.setName(request.getName());
+                    product.setDescription(request.getDescription());
+                    product.setPrice(request.getPrice());
+                    product.setCategory(category);
+                    product.setStatus(Boolean.TRUE);
+                    return productRepository.save(product);
+                })
+                .map(productMapper::toProductResponse)
+                .orElseThrow(CategoryNotFoundException::new);
     }
 
     @Override
     public ProductResponse update(Long id, CreateProductRequest request) {
-        return null;
+        return productRepository.findById(id)
+                .map(product -> categoryRepository
+                        .findById(request.getCategoryId())
+                        .map(category -> {
+                            product.setName(request.getName());
+                            product.setDescription(request.getDescription());
+                            product.setPrice(request.getPrice());
+                            product.setCategory(category);
+                            return productRepository.save(product);
+                        })
+                        .orElseThrow(CategoryNotFoundException::new)
+                )
+                .map(productMapper::toProductResponse)
+                .orElseThrow(ProductNotFoundException::new);
     }
 
     @Override
     public void deleteById(Long id) {
+        if (productRepository.findById(id).isEmpty()) throw new ProductNotFoundException();
 
+        productRepository.deleteById(id);
     }
 }
